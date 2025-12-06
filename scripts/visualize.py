311c2646b3fae65d8ca9f89059081acd6826bdcb
Sven Roediger <sroediger@g.ucla.edu>
2025-12-05 21:15:39 -0800

    UMAP for ScopeBO

---
+++ scripts/visualize.py
@@ -0,0 +1,181 @@
+import ast
+import colorsys
+import os
+from pathlib import Path
+import re
+from statistics import stdev
+import sys
+
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import scale
+from umap import UMAP
+
+
+# General plt parameters
+plt.rcParams.update({
+    "axes.titlesize": 20,        # Subplot title
+    "axes.labelsize": 16,        # X and Y labels
+    "figure.titlesize": 24,      # Suptitle
+    "xtick.labelsize": 14,       # X tick labels
+    "ytick.labelsize": 14,       # Y tick labels
+    "legend.fontsize": 14,       # Legend text
+    "legend.title_fontsize": 14, # Legend titles
+    "font.family": "Helvetica"   # Font
+    })
+
+# Define a colormap for plotting
+doyle_colors = ["#CE4C6F", "#1561C2", "#188F9D","#C4ADA2","#515798", "#CB7D85", "#A9A9A9"]
+colors = [doyle_colors[1],"#FFFFFFD1",doyle_colors[0]]
+cont_cmap = LinearSegmentedColormap.from_list("Doyle_cont", colors)
+
+def UMAP_view(filename,
+              obj_to_show = None,
+              obj_bounds = None,
+              objectives = None,
+              display_cut_samples = True,
+              figsize = (10,8),
+              dpi = 600,
+              show_figure = True,
+              cbar_title = None,
+              return_dfs = False,
+              directory = "."):
+    """
+    Creates a UMAP for the search space, highlighting the picked samples.
+    ----------
+    filename : str or Path
+        Path to the CSV file containing the reaction search space.
+    obj_to_show : str or None
+    Name of the objective that is visualized.
+    If None (Default), the first listed objective is used.
+
+    obj_bounds : tuple or list, optional
+        (max, min) values to manually set the colorbar range for `obj_to_show`.
+        If None, the min/max are taken from the observed evaluated samples.
+
+    objectives : list-like, optional
+        List of column names containing objective values (including "PENDING").
+        If None, they are automatically inferred from columns containing
+        "PENDING" strings.
+
+    display_cut_samples : bool, default=True
+        Whether cut samples (priority = -1) are shown as X markers.
+        If False, they are plotted as unseen points.
+
+    figsize : tuple, default=(10, 8)
+        Size of the generated UMAP figure in inches.
+
+    dpi : int, default=600
+        Resolution of the output figure.
+
+    show_figure : bool, default=True
+        Whether to display the UMAP plot.
+
+    cbar_title : str, optional
+        Custom title for the colorbar. If None, uses the objective name.
+
+    return_dfs : bool, default=False
+        If True, returns a dictionary of DataFrames for:
+            - seen     (evaluated samples)
+            - neutral  (unseen priority = 0)
+            - cut      (unseen priority = -1)
+
+    directory : str or Path, default="."
+        Directory containing the CSV file.
+    """
+
+    # Set directory.
+    wdir = Path(directory)
+
+    # read the results file
+    df_scope = pd.read_csv(wdir / filename, index_col=0, header=0)
+    df_scope["priority"] = df_scope["priority"].astype(float)
+
+    # identify the objectives (containing PENDING entries) if none are given
+    if objectives is None:
+        objectives = df_scope.columns[df_scope.eq("PENDING").any()].to_list()
+
+    # show the first objective in the UMAP if none has been specified in the function input
+    if obj_to_show is None:
+        obj_to_show = objectives[0]
+
+    
+    # scale the featurization data
+    df_scaled = df_scope.copy(deep=True)
+    df_scaled.drop(columns=objectives, inplace=True)
+    df_scaled = pd.DataFrame(scale(df_scaled),
+                             df_scaled.index,
+                             df_scaled.columns)
+
+    # create a UMAP
+    fit = UMAP(n_neighbors=40, 
+               min_dist=0.7,
+               n_components=2,
+               metric="euclidean",
+               random_state=42)
+    df_umap = pd.DataFrame(fit.fit_transform(df_scaled), 
+                           index = df_scaled.index,
+                           columns = ["UMAP1","UMAP2"])
+    df_umap.index = df_umap.index.astype(str)
+    
+    # add the result labels and the sample status ("priority")
+    df_umap["labels"] = df_scope[obj_to_show]
+    df_umap["priority"] = df_scope["priority"]
+
+    # separate the labelled and unlabelled samples
+    df_seen = df_umap.loc[df_umap["labels"] != "PENDING"].copy()
+    df_seen["labels"] = df_seen["labels"].astype(float)
+    df_unseen = df_umap.loc[df_umap["labels"] == "PENDING"]
+
+    df_neutral = df_unseen[df_unseen["priority"] == 0].copy()
+    df_cut = df_unseen[df_unseen["priority"] == -1].copy()
+
+    # print the UMAP if requested
+    if show_figure:
+        plt.figure(figsize=figsize, dpi = dpi, constrained_layout = True)
+
+        colormap = cont_cmap
+        if obj_bounds is None:
+            vmin = df_seen["labels"].min()
+            vmax = df_seen["labels"].max()
+        else:
+            vmin = obj_bounds[1]
+            vmax = obj_bounds[0]
+        norm = plt.Normalize(vmin,vmax)
+
+        # plot the neutral points
+        plt.scatter(
+            df_neutral["UMAP1"], df_neutral["UMAP2"], s=40, 
+            linewidth=0.3, edgecolor="k", color=doyle_colors[6],
+            marker="o", alpha = 0.8, zorder=1)
+
+        # check if cut samples should be highlighted
+        if display_cut_samples:
+            # plot the cut samples
+            plt.scatter(
+                df_cut["UMAP1"], df_cut["UMAP2"], s=100, edgecolor="k", marker = "X",
+                color=doyle_colors[4], alpha=0.6, linewidth = 0.3, zorder=2)
+        else:
+            # plot the cut samples the same way as the neutral samples
+            plt.scatter(
+                df_cut["UMAP1"], df_cut["UMAP2"], s=40, edgecolor="k", marker = "o",
+                color=doyle_colors[3], alpha=0.8, linewidth = 0.3, zorder=2)
+        
+        # plot the selected samples
+        scatter_numeric = plt.scatter(df_seen["UMAP1"], df_seen["UMAP2"], c=df_seen["labels"],
+                                        cmap=cont_cmap, norm=norm,s=250, alpha=1, edgecolor='k', linewidth=2)
+
+        cbar = plt.colorbar(scatter_numeric)
+        if cbar_title is None:
+            cbar_label = obj_to_show.capitalize()
+            cbar.set_label(cbar_label)
+        else:
+            cbar.set_label(cbar_title)
+
+        plt.xlabel("UMAP1")
+        plt.ylabel("UMAP2")
+        plt.show()
+
+    if return_dfs:
+        return {"seen": df_seen, "neutral": df_neutral, "cut": df_cut}
